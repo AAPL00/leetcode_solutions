b0d7738a5b9aa1f926de12c365da5bb21b049f22
AAPL00 <antonioalejandroperezlorenzo9@gmail.com>
2024-10-23 09:14:44 -0400

    Add two incomplete solutions

--- a.js
+++ a.js
@@ -1,35 +1,62 @@
 var isValid = function(s) {
-    var opened = [];
-    var closed = [];
     var validated = true;
-    for (var i = 0; i < s.length; i++) {
-        if (s[i] == '(') {
-            opened.push(s[i]);
-        }
-        else if (s[i] == '[') {
-            opened.push(s[i]);
-        }
-        else if (s[i] == '{') {
-            opened.push(s[i]);
-        }
-        else if (s[i] == ')') {
-            closed.push(s[i])
+    for (var i = 0; i < s.length; i++) {c
+        if (s[i] == ')') {
+            for (var j = i; j >= 0; j-- ) {
+                console.log(j);
+                if(s[j] == '(') {
+                    s[i] = '0';
+                    s[j] = '0';
+                    break;
+                }
+                else if (s[j] == '[' | s[j] == '{') {
+                    console.log("pene");
+                    validated = false;
+                    break;
+                }
+                else if (j == 0) {
+                    console.log(3);
+                    validated = false;
+                    break;
+                }
+            }
         }
         else if (s[i] == ']') {
-            closed.push(s[i]);
+            for (var j = i; j >= 0; j-- ) {
+                if(s[j] == '[') {
+                    console.log("aqui");
+                    s[i] = '0';
+                    s[j] = '0';
+                    break;
+                }
+                else if (s[j] == '(' | s[j] == '{') {
+                    validated = false;
+                    break;
+                }
+                else if (j == 0) {
+                    validated = false;
+                    break;
+                }
+            }
         }
         else if (s[i] == '}') {
-            closed.push(s[i]);
+            for (var j = i; j >= 0; j-- ) {
+                if(s[j] == '{') {
+                    s[i] = '0';
+                    s[j] = '0';
+                    break;
+                }
+                else if (s[j] == '[' | s[j] == '(') {
+                    validated = false;
+                    break;
+                }
+                else if (j == 0) {
+                    validated = false;
+                    break;
+                }
+            }
         }
     }
-    if (opened.length == closed.length) {
-        validated = false;
-    }
-    while (opened.is) {
-        if (opened.push() != closed.shift()) {
-            validated = false;
-            break;
-        }
-    }
-    return validated
-}
+    return validated;
+};
+console.log(isValid("([])"));
---
+++ b.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+var mergeTwoLists = function(list1, list2) {
+    var temp1;
+    var temp2;
+    var temp3;
+
+    while (list1.next != undefined) {
+        while (list2.next != undefined) {
+            if ()
+        }
+    }
+};
