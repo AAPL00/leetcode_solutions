08028a119aae248841b3a15eb250594c0af359a0
aapl00 <antonioalejandroperezlorenzo9@gmail.com>
2024-12-16 20:57:32 -0500

    XD

---
+++ 9. Palindrome Number.py
@@ -0,0 +1,8 @@
+def isPalindrome(x):
+    result = True
+    number_str = str(x)
+    for index in range(len(number_str)):
+        if number_str[index] != number_str[-index - 1]:
+            result = False
+            break
+    return result
